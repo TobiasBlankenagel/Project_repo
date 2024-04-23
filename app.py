41c0bcd3b8f09a8dcd4ec8263983c9f35c20ee16
TobiasBlankenagel <167626394+TobiasBlankenagel@users.noreply.github.com>
2024-04-23 16:49:18 +0200

    2

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ def fetch_airport_data(location):
     url = "https://travel-advisor.p.rapidapi.com/airports/search"
     querystring = {"query": location, "locale": "en_US"}
     headers = {
-        "X-RapidAPI-Key": "20c5e19a55msh027a6942760467ap12650bjsne0765678bd0a",
-        "X-RapidAPI-Host": "travel-advisor.p.rapidapi.com"
+	"X-RapidAPI-Key": "20c5e19a55msh027a6942760467ap12650bjsne0765678bd0a",
+	"X-RapidAPI-Host": "travel-advisor.p.rapidapi.com"
     }
     response = requests.get(url, headers=headers, params=querystring)
     return response.json()
