03a6eb76ee68638dd084b7ebf6d19c4db4607886
TobiasBlankenagel <167626394+TobiasBlankenagel@users.noreply.github.com>
2024-04-23 09:35:12 +0200

    t

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def fetch_autocomplete_data(query):
     url = "https://skyscanner80.p.rapidapi.com/api/v1/flights/auto-complete"
     querystring = {"query": query, "market": "US", "locale": "en-US"}
     headers = {
-        "X-RapidAPI-Key": "20c5e19a55msh027a6942760467ap12650bjsne0765678bd0a",
+        "X-RapidAPI-Key": "YOUR_API_KEY",
         "X-RapidAPI-Host": "skyscanner80.p.rapidapi.com"
     }
     response = requests.get(url, headers=headers, params=querystring)
@@ -17,7 +17,6 @@ def display_airports(data):
     if 'data' in data:
         airports = [item for item in data['data'] if item["navigation"]["entityType"] == "AIRPORT"]
         if airports:
-            df = pd.DataFrame(airports)
             st.write("Wählen Sie einen Flughafen aus der Liste:")
             option = st.selectbox('Flughäfen:', airports, format_func=lambda x: f"{x['presentation']['title']} ({x['navigation']['localizedName']})")
             return option
@@ -37,23 +36,29 @@ def fetch_flights(from_id, depart_date):
         "locale": "en-US"
     }
     headers = {
-        "X-RapidAPI-Key": "20c5e19a55msh027a6942760467ap12650bjsne0765678bd0a",
+        "X-RapidAPI-Key": "YOUR_API_KEY",
         "X-RapidAPI-Host": "skyscanner80.p.rapidapi.com"
     }
     response = requests.get(url, headers=headers, params=querystring)
+    if response.status_code != 200:
+        st.error(f"Fehler bei der Flugabfrage: {response.status_code} - {response.text}")
+        return None
     return response.json()
 
 def display_flights(flights_data):
-    if 'results' in flights_data:
-        flights = flights_data['results']
-        if flights:
-            df = pd.DataFrame(flights)
-            st.write("Nächste Flüge vom ausgewählten Flughafen:")
-            st.dataframe(df)
+    if flights_data:
+        if 'results' in flights_data:
+            flights = flights_data['results']
+            if flights:
+                df = pd.DataFrame(flights)
+                st.write("Nächste Flüge vom ausgewählten Flughafen:")
+                st.dataframe(df)
+            else:
+                st.error("Keine Flüge gefunden.")
         else:
-            st.error("Keine Flüge gefunden.")
+            st.error("Fehler in den Flugdaten.")
     else:
-        st.error("Fehler bei der Flugabfrage.")
+        st.error("Keine Antwortdaten erhalten.")
 
 def main():
     st.title('Auto-Complete Suche für Flughäfen und Flüge')
@@ -69,25 +74,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
