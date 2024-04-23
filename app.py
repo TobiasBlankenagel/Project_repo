7ade6c15ea4f8539a7d96358ce211e820bacccdb
TobiasBlankenagel <167626394+TobiasBlankenagel@users.noreply.github.com>
2024-04-23 13:04:25 +0200

    t

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import requests
 from datetime import date
 
+# Fetches autocomplete data for flight locations based on user input
 def fetch_autocomplete_data(query):
     url = "https://skyscanner80.p.rapidapi.com/api/v1/flights/auto-complete"
     querystring = {"query": query, "market": "US", "locale": "en-US"}
@@ -12,6 +13,7 @@ def fetch_autocomplete_data(query):
     response = requests.get(url, headers=headers, params=querystring)
     return response.json()
 
+# Fetches flight data from the Skyscanner API based on the given airport ID and departure date
 def fetch_flights(from_id, depart_date):
     url = "https://skyscanner80.p.rapidapi.com/api/v1/flights/search-everywhere"
     querystring = {
@@ -29,25 +31,27 @@ def fetch_flights(from_id, depart_date):
     response = requests.get(url, headers=headers, params=querystring)
     return response.json() if response.status_code == 200 else None
 
-def display_autocomplete_results(data):
+def process_and_save_autocomplete_data(data):
     if data and 'data' in data and data['data']:
         st.write("Found airports and related entities:")
+        airports = []
         for item in data['data']:
-            st.write(f"ID: {item.get('id', 'N/A')}, Title: {item['presentation']['title']}, Subtitle: {item['presentation']['subtitle']}")
-    else:
-        st.error("No data found or unexpected response structure.")
-
-def save_airports(data):
-    if 'data' in data:
-        airports = [{'id': item['id'], 'title': item['presentation']['title']} for item in data['data'] if item['navigation']['entityType'] == 'AIRPORT']
+            id = item.get('id', 'N/A')
+            title = item['presentation']['title']
+            subtitle = item['presentation']['subtitle']
+            st.write(f"ID: {id}, Title: {title}, Subtitle: {subtitle}")
+            if item['navigation']['entityType'] == 'AIRPORT':
+                airports.append({'id': id, 'title': title})
         if airports:
             st.session_state['airports'] = airports
             st.write("Airports saved!")
         else:
             st.write("No airports to save found.")
     else:
-        st.error("No data to save available.")
+        st.error("No data found or unexpected response structure.")
 
+
+# Fetches flight information for all saved airports on the selected date
 def fetch_all_flights():
     if 'airports' in st.session_state and st.session_state['airports'] and 'depart_date' in st.session_state:
         flights_info = []
@@ -64,6 +68,7 @@ def fetch_all_flights():
     else:
         st.error("No saved airports or date. Please save airports and select a date.")
 
+# Main function to run the Streamlit application
 def main():
     st.title('Auto-Complete Search for Flights from a Location')
     if 'airports' not in st.session_state:
