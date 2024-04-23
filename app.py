115a4660fccec8b98cbbea73a2e43b8af0cef3a4
TobiasBlankenagel <167626394+TobiasBlankenagel@users.noreply.github.com>
2024-04-23 16:47:37 +0200

    2

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
+import streamlit as st
 import requests
 
-url = "https://travel-advisor.p.rapidapi.com/airports/search"
+def fetch_airport_data(location):
+    url = "https://travel-advisor.p.rapidapi.com/airports/search"
+    querystring = {"query": location, "locale": "en_US"}
+    headers = {
+        "X-RapidAPI-Key": "20c5e19a55msh027a6942760467ap12650bjsne0765678bd0a",
+        "X-RapidAPI-Host": "travel-advisor.p.rapidapi.com"
+    }
+    response = requests.get(url, headers=headers, params=querystring)
+    return response.json()
 
-querystring = {"query":"new york","locale":"en_US"}
+def main():
+    st.title('Airport Search')
+    location = st.text_input('Enter a location:', 'New York')
+    if st.button('Search'):
+        results = fetch_airport_data(location)
+        st.write(results)
 
-headers = {
-	"X-RapidAPI-Key": "20c5e19a55msh027a6942760467ap12650bjsne0765678bd0a",
-	"X-RapidAPI-Host": "travel-advisor.p.rapidapi.com"
-}
-
-response = requests.get(url, headers=headers, params=querystring)
-
-print(response.json())
+if __name__ == "__main__":
+    main()
