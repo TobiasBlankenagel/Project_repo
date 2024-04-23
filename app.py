aa6b55e4aed5e57f818fa452a67556179491455d
TobiasBlankenagel <167626394+TobiasBlankenagel@users.noreply.github.com>
2024-04-23 14:26:42 +0200

    r

--- app.py
+++ app.py
@@ -79,11 +79,7 @@ def fetch_all_flights():
 # entityid codes auflisten
 def fetch_geo_data(locale):
     url = f"https://partners.api.skyscanner.net/apiservices/v3/geo/hierarchy/flights/{locale}"
-    headers = {
-        "Accept": "application/json",
-        "x-api-key": "20c5e19a55msh027a6942760467ap12650bjsne0765678bd0a"  # Your API key
-    }
-    response = requests.get(url, headers=headers)
+    response = requests.get(url)
     
     if response.status_code == 401:
         st.error("Unauthorized. Check your API key and permissions.")
