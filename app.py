0fb8fa2465d792b31a0a12338199c141723d74eb
TobiasBlankenagel <167626394+TobiasBlankenagel@users.noreply.github.com>
2024-04-23 17:19:12 +0200

    2

--- app.py
+++ app.py
@@ -1,31 +1,26 @@
 import streamlit as st
 import requests
 
-def fetch_flight_destinations(query):
-    url = "https://booking-com15.p.rapidapi.com/api/v1/flights/searchDestination"
+def fetch_airport_data(query):
+    url = "https://tripadvisor16.p.rapidapi.com/api/v1/flights/searchAirport"
     querystring = {"query": query}
     headers = {
         "X-RapidAPI-Key": "20c5e19a55msh027a6942760467ap12650bjsne0765678bd0a",
-        "X-RapidAPI-Host": "booking-com15.p.rapidapi.com"
+        "X-RapidAPI-Host": "tripadvisor16.p.rapidapi.com"
     }
-    try:
-        response = requests.get(url, headers=headers, params=querystring)
-        if response.status_code == 200:
-            return response.json()
-        else:
-            return {"error": f"API returned a status code {response.status_code}: {response.text}"}
-    except Exception as e:
-        return {"error": str(e)}
+    response = requests.get(url, headers=headers, params=querystring)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return {"error": f"API returned a status code {response.status_code}: {response.text}"}
 
 def main():
-    st.title('Flight Destination Search')
-    query = st.text_input('Enter a keyword for flight destinations', 'New')
-    if st.button('Search Destinations'):
-        results = fetch_flight_destinations(query)
+    st.title('Airport Information Search')
+    query = st.text_input('Enter a city to search for its airports', 'London')
+    if st.button('Search Airports'):
+        results = fetch_airport_data(query)
         if 'error' in results:
             st.error("Failed to fetch data: " + results['error'])
-        elif results.get('status') == False:
-            st.error(f"Error from API: {results.get('message', 'No error message provided.')}")
         else:
             st.write(results)
 
