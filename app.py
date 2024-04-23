fdb71009ad56de0a698e55d12d6b8ca8e0816800
TobiasBlankenagel <167626394+TobiasBlankenagel@users.noreply.github.com>
2024-04-23 09:22:30 +0200

    t

--- app.py
+++ app.py
@@ -19,26 +19,28 @@ def fetch_autocomplete_data(query):
     
 # Daten zur Eingabe finden
 
-def display_results(data):
-    if data and 'data' in data:
-
+def display_results(raw_response):
+    if raw_response.status_code == 200:
         data = raw_response.json()
         st.write("Gesamte Antwortdaten:")
         st.json(data)  # Zeigt die gesamten JSON-Daten in einer ansprechbaren Form an
 
-
-        items = data['data']
-        df = pd.DataFrame.from_records([{
-            "Title": item["presentation"]["title"],
-            "Suggestion Title": item["presentation"]["suggestionTitle"],
-            "Subtitle": item["presentation"]["subtitle"],
-            "Localized Name": item["navigation"]["localizedName"],
-            "Entity Type": item["navigation"]["entityType"],
-            "Entity ID": item["navigation"]["entityId"],
-        } for item in items])
-        st.table(df)
+        if 'data' in data:
+            items = data['data']
+            df = pd.DataFrame.from_records([{
+                "Title": item["presentation"]["title"],
+                "Suggestion Title": item["presentation"]["suggestionTitle"],
+                "Subtitle": item["presentation"]["subtitle"],
+                "Localized Name": item["navigation"]["localizedName"],
+                "Entity Type": item["navigation"]["entityType"],
+                "Entity ID": item["navigation"]["entityId"],
+                "From ID": item["navigation"]["fromID"]
+            } for item in items if "navigation" in item])
+            st.table(df)
+        else:
+            st.error("Keine Ergebnisse gefunden.")
     else:
-        st.error("Keine Ergebnisse gefunden.")
+        st.error(f"Fehler {raw_response.status_code}: {raw_response.text}")
 
 # Eingabefeld
 
