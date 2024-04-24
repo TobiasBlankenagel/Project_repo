3115e1c9d70d85da2ef73012dd810eea74713ed5
TobiasBlankenagel <167626394+TobiasBlankenagel@users.noreply.github.com>
2024-04-24 09:33:47 +0200

    2

--- app.py
+++ app.py
@@ -24,25 +24,45 @@ def process_and_collect_locations(data):
                 location_info.append((city_country, iata_code))
     return location_info
 
+# Abfrage der Flugdaten für ein bestimmtes Datum und einen IATA-Code
+def fetch_flights(departure_date, iata_code):
+    url = "https://flight-info-api.p.rapidapi.com/schedules"
+    querystring = {
+        "version": "v2",
+        "DepartureDateTime": departure_date,
+        "DepartureAirport": iata_code,
+        "CodeType": "IATA",
+        "ServiceType": "Passenger"
+    }
+    headers = {
+        "X-RapidAPI-Key": "20c5e19a55msh027a6942760467ap12650bjsne0765678bd0a",
+        "X-RapidAPI-Host": "flight-info-api.p.rapidapi.com"
+    }
+    response = requests.get(url, headers=headers, params=querystring)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return {"error": "Unable to fetch data", "status_code": response.status_code}
+
 # Hauptfunktion zum Laufen auf Streamlit
 def main():
-    st.title('Auto-Complete Suche für Flughäfen')
+    st.title('Auto-Complete Suche für Flughäfen und Flugdatenabfrage')
 
     query = st.text_input('Geben Sie einen Standort ein', '')
+    departure_date = st.date_input('Wählen Sie ein Abflugdatum', min_value=date.today())
+
     if query:
         autocomplete_data = fetch_autocomplete_data(query)
         if autocomplete_data:
             location_info = process_and_collect_locations(autocomplete_data)
             if location_info:
-                # Erstellt eine Auswahlliste mit Flughafenname und speichert den IATA-Code
                 options = [info[0] for info in location_info]
                 index_selected = st.selectbox("Wählen Sie einen Flughafen:", options, format_func=lambda x: x)
                 if st.button("Auswahl bestätigen"):
-                    # Findet den IATA-Code für den ausgewählten Flughafen
                     iata_code = location_info[options.index(index_selected)][1]
                     st.write(f"Sie haben {index_selected} ausgewählt. IATA-Code: {iata_code}")
-                    # Optional: Speichern des IATA-Codes für weitere Verwendung
-                    st.session_state['iata_code'] = iata_code
+                    flights_data = fetch_flights(departure_date.isoformat(), iata_code)
+                    st.json(flights_data)
             else:
                 st.write("Keine Flughäfen gefunden.")
         else:
