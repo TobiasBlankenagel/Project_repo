eedf40eb9b7bc9b284d6b74f58d7a1e8cac96157
TobiasBlankenagel <167626394+TobiasBlankenagel@users.noreply.github.com>
2024-04-24 09:18:57 +0200

    2

--- app.py
+++ app.py
@@ -16,15 +16,33 @@ def fetch_autocomplete_data(query):
 # Speichert die gefundenen Airports ab und zeigt nur die EntityIDs an
 def process_and_display_entity_ids(data):
     if data and 'data' in data:
-        st.write("Gefundene Flughäfen und deren Entity IDs:")
+        countries = set()
         for item in data['data']:
             if 'navigation' in item and item['navigation']['entityType'] == 'AIRPORT':
-                IATA_id = item['navigation']['relevantFlightParams']['skyId']
-                name = item['presentation']['title']
-                subtitle = item['presentation']['subtitle']
-                st.write(f"{name} - {subtitle}: IATA = {IATA_id}")
+                country = item['presentation']['subtitle']
+                countries.add(country)
+
+        if len(countries) > 1:
+            country_selected = st.selectbox("Wählen Sie ein Land:", list(countries))
+            display_airports_by_country(data, country_selected)
+        elif countries:
+            display_airports_by_country(data, list(countries)[0])
     else:
         st.error("Keine Daten gefunden oder unerwartete Antwortstruktur.")
+
+# Zeigt die Flughäfen an, die zu dem ausgewählten Land gehören
+def display_airports_by_country(data, country_selected):
+    st.write(f"Gefundene Flughäfen in {country_selected} und deren IATA-Codes:")
+    iata_codes = []
+    for item in data['data']:
+        if 'navigation' in item and item['navigation']['entityType'] == 'AIRPORT':
+            subtitle = item['presentation']['subtitle']
+            if subtitle == country_selected:
+                IATA_id = item['navigation']['relevantFlightParams']['skyId']
+                name = item['presentation']['title']
+                st.write(f"{name}: IATA = {IATA_id}")
+                iata_codes.append(IATA_id)
+    st.session_state['iata_codes'] = iata_codes
 
 # Hauptfunktion zum Laufen auf Streamlit
 def main():
