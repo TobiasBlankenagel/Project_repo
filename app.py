35777efb2e10b4c62d20b70a5c240aebeef6f403
TobiasBlankenagel <167626394+TobiasBlankenagel@users.noreply.github.com>
2024-04-25 13:18:22 +0200

    2

--- app.py
+++ app.py
@@ -91,7 +91,6 @@ def get_weather(lat, lon):
         "lang": "de"  # Ergebnisse auf Deutsch
     }
     response = requests.get(url, params=params)
-    st.json(response)
     if response.status_code == 200:
         weather = response.json()
         temperature = weather['main']['temp']  # Temperatur in Celsius
