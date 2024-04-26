96d1fd4dea62897dd5538e4570a82067d839798f
TobiasBlankenagel <167626394+TobiasBlankenagel@users.noreply.github.com>
2024-04-26 12:13:00 +0200

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def fetch_autocomplete_data(query):
     }
     time.sleep(1)  # Verzögerung, um weniger wie ein Bot zu wirken
     response = requests.get(url, headers=headers, params=querystring)
-    st.json(response)
+    st.write(response)
     if response.status_code == 200:
         data = response.json()
         st.json(data)
