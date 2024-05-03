28e6a861fce1783048d98904616989b398cacf86
TobiasBlankenagel <167626394+TobiasBlankenagel@users.noreply.github.com>
2024-05-03 11:51:34 +0200

    3

--- app.py
+++ app.py
@@ -304,16 +304,20 @@ def suche_fluege():
                 progress.progress(100)  # Kompletter Fortschritt
                 if gefilterte_fluege:
                     st.write("Gefilterte Flüge gefunden:")
-                    for flug in sortierte_fluege:
+                    for index, flug in enumerate(sortierte_fluege):
+                        expander_key = f"expander_{flug['IATA']}_{flug['IATA_dep']}"
+                        expanded = st.session_state.get(expander_key, False)
                         with st.expander(f"Flug nach {flug['Zielort']}, {flug['Zielland']} bei {flug['Temperatur (C)']}°C"):
-                            st.write(f"Abflugzeit (lokal): {flug['Abflugzeit (lokal)']}")
-                            st.write(f"Wetter: {flug['Wetterzustand']} bei {flug['Temperatur (C)']} °C")
-                            st.write(f"Entfernung: {flug['Entfernung']} km")
-                            st.write(f"Preis: {flug['Preis']}")
-
-                            if st.button("Mehr Details", key=f"{flug['IATA']}_{flug['IATA_dep']}"):
-                                Preis = get_price(flug['IATA_dep'], flug['IATA'])  # Stelle sicher, dass die IATA-Codes korrekt angegeben sind
+                           # Zeige Fluginformationen an
+                            if st.button("Mehr Details", key=f"details_{flug['IATA']}_{flug['IATA_dep']}"):
+                                Preis = get_price(flug['IATA_dep'], flug['IATA'])
+                                st.session_state[expander_key] = True
                                 st.write(f"Preis für Flug von {flug['IATA_dep']} nach {flug['IATA']}: {Preis}")
+                            else:
+                                # Setze hier den Standardinhalt des Expanders
+                                st.write(f"Abflugzeit (lokal): {flug['Abflugzeit (lokal)']}")
+                                st.write(f"Wetter: {flug['Wetterzustand']} bei {flug['Temperatur (C)']} °C")
+                                st.write(f"Entfernung: {flug['Entfernung']} km")
                 else:
                     st.write("Keine Flüge gefunden, die den Temperaturkriterien entsprechen.")
             else:
